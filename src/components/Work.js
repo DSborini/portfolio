546ab95177329ac60f7f2a35f8dd74a081633e23
Daniel <dsouzaborini@gmail.com>
2021-09-28 19:13:06 -0300

    first commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,7 +5,6 @@ import content from '../content';
 import useWindowPosition from '../hook/useWindowPosition';
 
 export default function Work() {
-  const animated = useWindowPosition('header', 0.6);
   return (
     <div
       className="min-h-screen flex justify-center items-center flex-col"
@@ -15,7 +14,8 @@ export default function Work() {
       id="mywork"
     >
       <h1 className="text-5xl font-dosis font-bold">{content.work.title}</h1>
-      <p className="text-gray-600 text-2xl font-dosis mb-10">I have done</p>
+      <p className="text-gray-600 text-2xl font-dosis mb-10">desenvolvidos</p>
+      {/* _________________________________________________________________________________________________project */}
       <div className="flex flex-col md:flex-row justify-between items-center w-11/12 ">
         <LazyLoadImage
           effect="blur"
@@ -28,9 +28,52 @@ export default function Work() {
           className="transtion duration-2000 ease-in-out p-10 max-w-xl lg:max-w-3xl rounded-lg hidden md:block"
           style={{
             border: '1px solid #e5ecf9',
-            transform: animated
-              ? 'translate(-10%, 0%) rotate3d(0.540, -0.95, 0, 22deg) rotateZ(7deg)'
-              : '',
+            boxShadow:
+              '35px 50px 90px -25px rgba(50, 50, 95, 0.5), 20px 35px 75px -35px rgba(0, 0, 0, 0.5)',
+          }}
+        >
+          <ProjectDetail />
+        </div>
+        <div className="flex justify-center items-center md:hidden">
+          <ProjectDetail />
+        </div>
+      </div>
+      {/* _________________________________________________________________________________________________project */}
+      <div className="flex flex-col md:flex-row justify-between items-center w-11/12 ">
+        <LazyLoadImage
+          effect="blur"
+          placeholderSrc={content.work.imgPlaceholder}
+          src={content.work.img}
+          alt="phone prototype"
+          className="m-10 transtion duration-2000 ease-in-out z-10  md:w-3/5 w-4/5"
+        />
+        <div
+          className="transtion duration-2000 ease-in-out p-10 max-w-xl lg:max-w-3xl rounded-lg hidden md:block"
+          style={{
+            border: '1px solid #e5ecf9',
+            boxShadow:
+              '35px 50px 90px -25px rgba(50, 50, 95, 0.5), 20px 35px 75px -35px rgba(0, 0, 0, 0.5)',
+          }}
+        >
+          <ProjectDetail />
+        </div>
+        <div className="flex justify-center items-center md:hidden">
+          <ProjectDetail />
+        </div>
+      </div>
+      {/* _________________________________________________________________________________________________project */}
+      <div className="flex flex-col md:flex-row justify-between items-center w-11/12 ">
+        <LazyLoadImage
+          effect="blur"
+          placeholderSrc={content.work.imgPlaceholder}
+          src={content.work.img}
+          alt="phone prototype"
+          className="m-10 transtion duration-2000 ease-in-out z-10  md:w-3/5 w-4/5"
+        />
+        <div
+          className="transtion duration-2000 ease-in-out p-10 max-w-xl lg:max-w-3xl rounded-lg hidden md:block"
+          style={{
+            border: '1px solid #e5ecf9',
             boxShadow:
               '35px 50px 90px -25px rgba(50, 50, 95, 0.5), 20px 35px 75px -35px rgba(0, 0, 0, 0.5)',
           }}
@@ -47,6 +90,11 @@ export default function Work() {
 
 const ProjectDetail = () => {
   const animated = useWindowPosition('header', 0.6);
+
+  const redirectToProject = (link) => {
+    window.location.assign(link);
+  }
+
   return (
     <div>
       <h1
@@ -63,18 +111,34 @@ const ProjectDetail = () => {
       >
         {content.work.desc}
       </p>
-      <button
-        className={`${
-          animated ? '' : 'translate-y-10 opacity-0'
-        } transform transition duration-2000  px-20 py-3 m-4 bg-black flex justify-around text-white rounded-lg shadow-2xl`}
-      >
-        <img
-          src="https://timo.engineer/assets/icons/rocket.svg"
-          alt="rocket"
-          className="mr-5"
-        />
-        <p className="text-lg">Website</p>
-      </button>
+      <div className="flex justify-start">
+        <button
+          onClick={ () => { redirectToProject('https://dsborini.github.io/recipes-app/') } }
+          className={`${
+            animated ? '' : 'translate-y-10 opacity-0'
+          } transform transition duration-2000  px-10 py-3 m-4 bg-black flex justify-around text-white rounded-lg shadow-2xl`}
+        >
+          <img
+            src="../assets/rocket.svg"
+            alt="rocket"
+            className="mr-5"
+          />
+          <p className="text-lg">Website</p>
+        </button>
+        <button
+          onClick={ () => { redirectToProject('https://github.com/DSborini/recipes-app') } }
+          className={`${
+            animated ? '' : 'translate-y-10 opacity-0'
+          } transform transition duration-2000  px-10 py-3 m-4 bg-black flex justify-around text-white rounded-lg shadow-2xl`}
+        >
+          <img
+            src="../assets/github-light.png"
+            alt="github"
+            className="mr-5"
+          />
+          <p className="text-lg">Código</p>
+        </button>
+      </div>
     </div>
   );
 };
