9a01c183154f86e90c841916349866e2e57be2e8
DSborini <DSborini@users.noreply.github.com>
2021-09-30 05:42:44 +0000

    deploy: f9c1a4ef31518a8b0cbcea561dd036522431995e

---
+++ precache-manifest.f57cf094530a5fdecee782293937dfd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8430bf286f8a90c9fe42dc27a8483d9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bcaeabb20a8512305020",
+    "url": "/portfolio/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "443470afa2eebfd44796",
+    "url": "/portfolio/static/css/main.9f8ab3bb.chunk.css"
+  },
+  {
+    "revision": "bcaeabb20a8512305020",
+    "url": "/portfolio/static/js/2.685ea4dc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.685ea4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443470afa2eebfd44796",
+    "url": "/portfolio/static/js/main.e0d3edde.chunk.js"
+  },
+  {
+    "revision": "e9eccba70682dbddac7d",
+    "url": "/portfolio/static/js/runtime-main.30f3f619.js"
+  }
+]);
